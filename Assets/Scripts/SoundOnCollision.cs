ea2f18cca8d280a925dd288671112df08ea7f1f5
Jonah Warren <44376983+playfulsystems@users.noreply.github.com>
2023-10-03 11:57:57 -0400

    updated for 2022.3.7

--- Assets/Scripts/SoundOnCollision.cs
+++ Assets/Scripts/SoundOnCollision.cs
@@ -5,28 +5,30 @@ using UnityEngine;
 public class SoundOnCollision : MonoBehaviour
 {
     public AudioSource audioSource;
-    int collisionPoints;
+    int collisionPointsPlayed;
 
     // play once on collision
     private void OnCollisionEnter(Collision collision)
     {
-        audioSource.pitch = Random.Range(0.8f, 1.2f);
-        audioSource.volume = 1;
-        audioSource.Play();
-        collisionPoints = 1;
+        if (Time.time > 0.5f)
+        {
+            audioSource.pitch = Random.Range(0.8f, 1.2f);
+            audioSource.volume = 1;
+            audioSource.Play();
+            collisionPointsPlayed = 1;
+        }
     }
 
     // play on each collision point
     private void OnCollisionStay(Collision collision)
     {
         // all contacts in a collision = collision.contacts
-        if (collision.contacts.Length > collisionPoints)
+        if (collision.contacts.Length > collisionPointsPlayed && Time.time > 0.5f)
         {
             audioSource.pitch = Random.Range(0.8f, 1.2f);
             audioSource.volume *= 0.75f;
             audioSource.Play();
+            collisionPointsPlayed++;
         }
-        collisionPoints = collision.contacts.Length;
-
     }
 }
